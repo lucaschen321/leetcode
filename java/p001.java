1b051afc6183dc44f450042072bf07fcf15151fa
Lucas Chen <lucaschen321@gmail.com>
2017-02-01 19:54:09 -0500

    P1: Added Java solution

---
+++ java/p001.java
@@ -0,0 +1,31 @@
+/* 
+ * Solution to Leetcode Problem 1
+ * Source: https://leetcode.com/problems/two-sum/
+ * Author: Lucas Chen
+ */
+
+/*
+ * We can make one pass through the array, iterating and inserting elements 
+ * into the hashtable, while checking if the current element's complement
+ * to the target exists in the table. If the complement exists, we are finished
+ * and we can return a solution. 
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+import java.util.*;
+
+public class p001 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement))
+                return new int[]{map.get(complement), i};
+            else
+                map.put(nums[i], i);
+        }
+        return new int[]{0, 0};
+    }
+}
